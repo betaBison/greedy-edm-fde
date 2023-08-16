6349e24e5eeba6ac9102c4c9d8516e7d2cfbc838
betaBison <knowles.derek@gmail.com>
2023-08-16 13:34:34 -0700

    test full thing

--- fde_simulated.py
+++ fde_simulated.py
@@ -12,8 +12,8 @@ import gnss_lib_py as glp
 import matplotlib.pyplot as plt
 
 locations = {
-              # "stanford_oval" : (37.42984154652992, -122.16946303566934, 0.),
-              # "munich" : (48.16985710449595, 11.551627945697028, 0.),
+              "stanford_oval" : (37.42984154652992, -122.16946303566934, 0.),
+              "munich" : (48.16985710449595, 11.551627945697028, 0.),
               "london" : (51.5097085796586, -0.16008158973060102, 0.),
               "hong_kong" : (22.327793473417067, 114.17122448832379, 0.),
               "zurich" : (47.407491810621345, 8.500756183071228, 0.),
@@ -40,6 +40,9 @@ for location_name, location_tuple in locations.items():
         data_45means[bias_value] = []
 
         i = 0
+        fault_gt = []
+        corr_pr_m = []
+        raw_pr_m = []
         for timestamp, _, navdata in full_data.loop_time("gps_millis"):
 
             # navdata = navdata.copy(cols=list(np.arange(10)))
@@ -49,16 +52,30 @@ for location_name, location_tuple in locations.items():
             # faulty_idx = list(np.random.randint(0,len(navdata),size=int(0.5*len(navdata))))
             rand_index_order = np.arange(len(navdata))
             np.random.shuffle(rand_index_order)
-            faulty_idx = list(rand_index_order)[:4]
-            print("fault:",faulty_idx,navdata["gnss_sv_id",faulty_idx])
+            faulty_idxs = list(rand_index_order)[:4]
+            # print("\n\n\nfault:",faulty_idxs,navdata["gnss_sv_id",faulty_idxs])
 
-            print("before",navdata["corr_pr_m"][faulty_idx] )
-            navdata["corr_pr_m",faulty_idx] += bias_value
-            navdata["raw_pr_m",faulty_idx] += bias_value
-            print("after",navdata["corr_pr_m"][faulty_idx])
-            _, data = glp.solve_fde(navdata,method="edm",max_faults=1,verbose=False)
-            data_45means[bias_value] += data["data_45means"]
+            # print("before",navdata["corr_pr_m"][faulty_idxs] )
+            navdata["corr_pr_m",faulty_idxs] += bias_value
+            navdata["raw_pr_m",faulty_idxs] += bias_value
+            corr_pr_m_subset = navdata["corr_pr_m"]
+            raw_pr_m_subset = navdata["raw_pr_m"]
+            # print("after",navdata["corr_pr_m"][faulty_idxs])
+
+            fault_gt_subset = np.array([0] * len(navdata))
+            fault_gt_subset[faulty_idxs] = 1
+            # print(fault_gt_subset)
+            fault_gt += list(fault_gt_subset)
+            corr_pr_m += list(corr_pr_m_subset)
+            raw_pr_m += list(raw_pr_m_subset)
             i += 1
+
+        full_data["fault_gt"] = fault_gt
+        full_data["corr_pr_m"] = corr_pr_m
+        full_data["raw_pr_m"] = raw_pr_m
+        
+        glp.evaluate_fde(full_data,method="edm",
+                                   threshold=1.5E9,verbose=True)
 
     labels = [str(b) + "m faults" for b in data_45means.keys()]
     plt.figure()
